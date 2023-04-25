e73b6f21d3eabfe121ecec663f919a556c8e4aae
Sonia <coleysonia@gmail.com>
2023-04-25 19:49:02 -0400

    p

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import Cart from "./Components/Cart";
+import Nav from "./Components/Nav";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
-    <div className="App">
-     Shopping Cart
+    <div className='App'>
+      <Nav />
+      <Cart />
+      <HomeScreen />
     </div>
   );
 }
